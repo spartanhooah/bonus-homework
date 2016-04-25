fa091e1f326d15073b72cbadaac97f141f9e098c
Ben Frey <spartanhooah@users.noreply.github.com>
2016-04-25 16:53:42 -0400

    Maybe finished computation loop.

--- stability.m
+++ stability.m
@@ -1,10 +1,20 @@
 clear
 load('ad_data.mat')
-lambdas = [0, 0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
-[~, bootstraps] = bootstrp(1000, @corr, X_train);
+lambdas = [0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
+[~, bootstraps] = bootstrp(1000, @mean, X_train);
+probabilities = zeros(size(X_train, 2), size(lambdas, 2));
+% Specify the options (use without modification).
+opts.rFlag = 1; % range of par within [0, 1].
+opts.tol = 1e-6; % optimization precision
+opts.tFlag = 4; % termination options.
+opts.maxIter = 5000; % maximum iterations.
 
 for lambda = lambdas
+    counts = zeros(size(X_train, 2), 1);
     for i = 1:1000
-        
+        [weights, ~] = LogisticR(X_train(bootstraps(:, i), :), y_train, lambda, opts);
+        selected = find(weights == 0);
+        counts(selected) = counts(selected) + 1;
     end
+    probabilities(lambdas == lambda) = counts / 1000;
 end
