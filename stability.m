5312d5bf39c33d76601f918f8551f29c68616edc
Ben Frey <spartanhooah@users.noreply.github.com>
2016-04-26 16:56:37 -0400

    Removed 0.01 from lambda vector since the lowest max probability across all lambdas was ~0.95.

--- stability.m
+++ stability.m
@@ -1,6 +1,6 @@
 clear
 load('ad_data.mat')
-lambdas = [0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
+lambdas = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
 [~, bootstraps] = bootstrp(1000, @mean, X_train);
 probabilities = zeros(size(X_train, 2), size(lambdas, 2));
 % Specify the options (use without modification).
@@ -13,8 +13,10 @@ for lambda = lambdas
     counts = zeros(size(X_train, 2), 1);
     for i = 1:1000
         [weights, ~] = LogisticR(X_train(bootstraps(:, i), :), y_train, lambda, opts);
-        selected = find(weights == 0);
+        selected = find(weights ~= 0);
         counts(selected) = counts(selected) + 1;
     end
-    probabilities(lambdas == lambda) = counts / 1000;
+    probabilities(:, lambdas == lambda) = counts / 1000;
 end
+
+total_probability = max(probabilities, [], 2);
