a8aec92e1fcc38636fed6318e5fac8210e5cb00d
Ben Frey <spartanhooah@gmail.com>
2016-04-27 10:32:15 -0400

    Fixed formatting of output.

---
+++ hardimpute.m
@@ -0,0 +1,9 @@
+function [X complete] = hardimpute(X_missing, Omega, r)
+% Input:
+% X_missing -- a m-by-n input matrix, only values at Omega
+% Omega -- a m-by-n binary matrix, indicating location of the missing values
+% r -- rank
+
+
+
+end
--- hardimpute_exp.m
+++ hardimpute_exp.m
@@ -5,3 +5,4 @@ temp = temp(:);
 [~, idx_to_remove] = datasample(1:size(temp, 1), size(temp, 1)/2, 'Replace', false);
 temp(idx_to_remove) = 0;
 data = reshape(temp, [128 128 1]);
+omega = data == 0;
