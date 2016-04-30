3aa220f0280880d66472fb3599c48b42531e6d49
Ben Frey <spartanhooah@gmail.com>
2016-04-30 09:04:49 -0400

    First attempt at the meat of hard impute.

--- hardimpute.m
+++ hardimpute.m
@@ -4,6 +4,16 @@ function [X complete] = hardimpute(X_missing, Omega, r)
 % Omega -- a m-by-n binary matrix, indicating location of the missing values
 % r -- rank
 
+P_X_Omega = X_missing;
+Z_new = zeros(size(X_missing));
+singulars = svds(P_X_Omega, r);
 
+for lambda = singulars
+    Z_old = Z_new;
+    Z_Omega_perp = zeros(size(Z_old));
+    Z_Omega_perp(~Omega) = Z_old(~Omega);
+    [U, S, V] = svds(P_X_Omega + Z_Omega_perp);
+    Z_new = U * S * V';
+end
 
 end
--- hardimpute_exp.m
+++ hardimpute_exp.m
@@ -1,8 +1,10 @@
 clear
 data = imread('Samoa.JPG');
+data = im2double(data);
 temp = data(:, :, 1);
 temp = temp(:);
 [~, idx_to_remove] = datasample(1:size(temp, 1), size(temp, 1)/2, 'Replace', false);
 temp(idx_to_remove) = 0;
 data = reshape(temp, [128 128 1]);
 omega = data == 0;
+r_arr = [1, 5, 10, 15, 20, 25, 30];
