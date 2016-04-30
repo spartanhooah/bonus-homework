220917c8c375d627f0a3e64045f720434cc67f59
Ben Frey <spartanhooah@gmail.com>
2016-04-30 09:26:33 -0400

    It runs, but doesn't work correctly. More modification needed.

--- hardimpute.m
+++ hardimpute.m
@@ -1,4 +1,4 @@
-function [X complete] = hardimpute(X_missing, Omega, r)
+function [X_complete] = hardimpute(X_missing, Omega, r)
 % Input:
 % X_missing -- a m-by-n input matrix, only values at Omega
 % Omega -- a m-by-n binary matrix, indicating location of the missing values
@@ -6,14 +6,24 @@ function [X complete] = hardimpute(X_missing, Omega, r)
 
 P_X_Omega = X_missing;
 Z_new = zeros(size(X_missing));
+Z_old = Z_new;
 singulars = svds(P_X_Omega, r);
+epsilon = 0.1;
+Z_star = {};
 
-for lambda = singulars
+for i = 1:size(singulars)
+    while true
+        Z_old = Z_new;
+        Z_Omega_perp = zeros(size(Z_old));
+        Z_Omega_perp(~Omega) = Z_old(~Omega);
+        [U, S, V] = svds(P_X_Omega + Z_Omega_perp);
+        Z_new = U * S * V';
+        if norm(Z_old - Z_new, 'fro')^2 / norm(Z_old, 'fro')^2 < epsilon
+            break
+        end
+    end
+    Z_star{i} = Z_new;
     Z_old = Z_new;
-    Z_Omega_perp = zeros(size(Z_old));
-    Z_Omega_perp(~Omega) = Z_old(~Omega);
-    [U, S, V] = svds(P_X_Omega + Z_Omega_perp);
-    Z_new = U * S * V';
 end
-
+X_complete = Z_new;
 end
--- hardimpute_exp.m
+++ hardimpute_exp.m
@@ -1,10 +1,21 @@
 clear
 data = imread('Samoa.JPG');
 data = im2double(data);
-temp = data(:, :, 1);
-temp = temp(:);
-[~, idx_to_remove] = datasample(1:size(temp, 1), size(temp, 1)/2, 'Replace', false);
-temp(idx_to_remove) = 0;
-data = reshape(temp, [128 128 1]);
-omega = data == 0;
+noisy = data(:, :, 1);
+subplot(1, 3, 1)
+imshow(noisy);
+noisy = noisy(:);
+[~, idx_to_remove] = datasample(1:size(noisy, 1), size(noisy, 1)/2, 'Replace', false);
+noisy(idx_to_remove) = 0;
+noisy = reshape(noisy, [128 128 1]);
+subplot(1, 3, 2)
+imshow(noisy)
+omega = noisy == 0;
 r_arr = [1, 5, 10, 15, 20, 25, 30];
+
+for r = r_arr
+    X_impute = hardimpute(noisy, omega, r);
+    subplot(1, 3, 3)
+    imshow(X_impute)
+    pause(3)
+end
