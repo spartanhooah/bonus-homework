189e3f7169fe53f3b655c9a794173bd98126d1eb
Ben Frey <spartanhooah@gmail.com>
2016-05-02 19:45:38 -0400

    Added code to reconstruct color images.

--- hardimpute_exp.m
+++ hardimpute_exp.m
@@ -7,28 +7,46 @@ long_data = long_data(:);
         'Replace', false);
 r_arr = [1, 5, 10, 15, 20, 25, 30];
 errors = zeros(3, size(r_arr, 2));
+X_impute = {};
 for i = 1:3
     noisy = data(:, :, i);
     figure
-    subplot(1, 3, 1)
-    imshow(data(:, :, i));
+    subplot(3, 3, 1)
+    imshow(data(:, :, i), 'InitialMagnification', 100);
     title1 = sprintf('Original channel %d', i);
     title(title1)
     noisy = noisy(:);
     noisy(idx_to_remove) = 0;
     noisy = reshape(noisy, [128 128 1]);
-    subplot(1, 3, 2)
-    imshow(noisy)
+    subplot(3, 3, 2)
+    imshow(noisy, 'InitialMagnification', 100)
     title('50% zeros')
     omega = noisy == 0;
 
     for j = 1:size(r_arr, 2)
-        X_impute = abs(hardimpute(noisy, omega, r_arr(j)));
-        subplot(1, 3, 3)
-        imshow(X_impute)
+        X_impute{i, j} = hardimpute(noisy, omega, r_arr(j));
+        subplot(3, 3, j + 2)
+        imshow(X_impute{i}, 'InitialMagnification', 100)
         title3 = sprintf('r = %d', r_arr(j));
         title(title3)
         pause(1)
-        errors(i, j) = norm(X_impute - noisy)^2;
+        errors(i, j) = norm(X_impute{i} - noisy)^2;
     end
+end
+
+colors = {};
+figure
+subplot(3, 3, 1)
+imshow(data, 'InitialMagnification', 100)
+title('Original Image')
+for i = 1:7
+    temp = zeros(size(data));
+    temp(:, :, 1) = X_impute{1, i};
+    temp(:, :, 2) = X_impute{2, i};
+    temp(:, :, 3) = X_impute{3, i};
+    colors{i} = temp;
+    subplot(3, 3, i+1)
+    imshow(temp, 'InitialMagnification', 100);
+    title4 = sprintf('r = %d', r_arr(i));
+    title(title4)
 end
