330a7d638b6b8b72dfef4753a76c62b9c5a397d9
Ben Frey <spartanhooah@gmail.com>
2016-05-04 14:21:53 -0400

    Added second index to imshow for X_impute.

--- hardimpute_exp.m
+++ hardimpute_exp.m
@@ -3,7 +3,7 @@ data = imread('Samoa.JPG');
 data = im2double(data);
 long_data = data(:, :, 1);
 long_data = long_data(:);
-[~, idx_to_remove] = datasample(1:size(long_data, 1), size(long_data, 1)/2,...
+[~, idx_to_remove] = datasample(1:size(long_data, 1), floor(size(long_data, 1)/5),...
         'Replace', false);
 r_arr = [1, 5, 10, 15, 20, 25, 30];
 errors = zeros(3, size(r_arr, 2));
@@ -20,13 +20,13 @@ for i = 1:3
     noisy = reshape(noisy, [128 128 1]);
     subplot(3, 3, 2)
     imshow(noisy, 'InitialMagnification', 100)
-    title('50% zeros')
+    title('20% zeros')
     omega = noisy == 0;
 
     for j = 1:size(r_arr, 2)
         X_impute{i, j} = hardimpute(noisy, omega, r_arr(j));
         subplot(3, 3, j + 2)
-        imshow(X_impute{i}, 'InitialMagnification', 100)
+        imshow(X_impute{i, j}, 'InitialMagnification', 100)
         title3 = sprintf('r = %d', r_arr(j));
         title(title3)
         pause(1)
